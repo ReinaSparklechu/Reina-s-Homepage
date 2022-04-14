2109335c0c5199c1c6e2a145b0b4da547a0ba00a
ReinaSparklechu <Sparklechus@gmail.com>
2022-04-14 17:25:36 +0800

    Update etch.js

--- etch.js
+++ etch.js
@@ -3,18 +3,21 @@ var cell = document.createElement('div');
 var panel = document.createElement('div');
 var column = document.createElement('div');
 var button = document.createElement('button')
-cell.style.border = 'solid 0.01px grey';
 cell.style.width= '10px';
 cell.style.height = '10px';
 cell.style.backgroundColor = 'black'
 cell.style.padding = "0";
+cell.style.margin = '0';
+column.style.padding= '0';
+column.style.margin = '0';
+
 cell.setAttribute('class', 'grid');
 righttab.style.display = 'flex';
 righttab.style.flexDirection='column';
 righttab.appendChild(panel);
 button.textContent = 'Refresh'
 // build column
-for(var i = 0; i<64; i++)
+for(var i = 0; i<16; i++)
 {
     const grid = cell.cloneNode(true);
     column.appendChild(grid);
@@ -22,7 +25,7 @@ for(var i = 0; i<64; i++)
 //set panel flex direction to row
 panel.style.flexDirection = 'row';
 panel.style.display = 'flex';
-for(var i=0; i<64; i++)
+for(var i=0; i<16; i++)
 {
     const columns = column.cloneNode(true);
     panel.appendChild(columns);
@@ -46,4 +49,6 @@ for( var i = 0; i<cells.length; i++)
 {
     const grid = cells[i];
     grid.addEventListener('click', () => grid.style.backgroundColor = 'white');
+    grid.addEventListener('dragover', () => grid.style.backgroundColor = 'white');
+    
 }
