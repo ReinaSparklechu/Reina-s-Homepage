78a8472edce156031e04f54f3579c46b74d695b1
ReinaSparklechu <Sparklechus@gmail.com>
2022-04-14 12:15:34 +0800

    Got a column of pixels to change colour

--- etch.js
+++ etch.js
@@ -1,7 +1,5 @@
 const righttab = document.getElementById('right');
-var panel = document.createElement('div');
 var cell = document.createElement('div');
-var row = document.createElement('div');
 cell.style.borderBlockColor = 'solid white';
 cell.style.borderWidth = '4px';
 cell.style.width= '20px';
@@ -9,22 +7,12 @@ cell.style.height = '20px';
 cell.style.backgroundColor = 'black'
 cell.style.padding = "0";
 cell.setAttribute('class', 'grid');
-cell.click.call()
-row.style.display = "flex";
-row.style.flexDirection = "row"
-panel.style.width = '320px';
-panel.style.height = '320px';
-panel.style.display ="flex";
-panel.style.flexDirection = "column"
-panel.style.justifyContent = "space-evenly";
-cell.addEventListener('click', () =>cell.style.background ='white');
-for(var i = 0; i<16; i++)
-{
-    row.appendChild(cell.cloneNode(true));
-}
-for(var i=0; i<16;i++)
-{
-    panel.appendChild(row.cloneNode(true));
-}
+cell.addEventListener('click', () => cell.style.backgroundColor ='white');
 
-righttab.appendChild(panel);
+for(i=0; i<16; i++)
+{
+    const grid = cell.cloneNode();
+    grid.addEventListener('click', () =>grid.style.backgroundColor ='white')
+    righttab.appendChild(grid);
+}
+righttab.appendChild(cell);
