6bbb46642e410d33eba9b946f4e9e3e4b0f07a39
ReinaSparklechu <Sparklechus@gmail.com>
2022-04-16 18:05:38 +0800

    Added word list for daily scrapper and filter

--- index.js
+++ index.js
@@ -13,13 +13,16 @@ admin.initializeApp({
 var db =admin.firestore();
 var today = Date();
 var allWords = [""];
+var filterWords = ['(v.redd.it)', '(i.redd.it)', 'the', 'then', 'this','much','was','were','this','that','they','them','their','his','her'
+,'not','for', 'The', 'are', 'you', 'don’t' , 'Not' , '[OC]','from', 'and', 'And', 'your','Your','can', 'who', 'Who', 'what', 'where', 'when', 'why', 'how' ];
 function scrapeReddit(){
     request('https://old.reddit.com/r/popular/?geo_filter=GLOBAL', (error, response, html)=> {
         if(!error && response.statusCode==200) {
             const $ = cheerio.load(html);
             $('p.title').each((i,title) =>
             {
-                var headings = $(title).text().trim();
+                // set all to lower case for easy filtering
+                var headings = $(title).text().trim().toLowerCase();
                 var link = $(title.firstChild).attr("href");
                 console.log(headings);
                 let redditHeading = {
@@ -31,12 +34,16 @@ function scrapeReddit(){
                 var words = headings.split(" ")
                 allWords = allWords.concat(words);
             })
-            allWords = allWords.filter(word =>word.length >4);
+            allWords = allWords.filter(word => word.length>2 && !filterWords.includes(word));
+            let collection = {
+                list: allWords
+            }
+            db.collection('reddit').add(collection);
             console.log(allWords);
         }
     });
 }
-const scrape = scrapeReddit();
+scrapeReddit();
 //var deletedata = db.collection('reddit').where('Date', '==', 'Thu Apr 14 2022 11:30:42 GMT+0800 (Singapore Standard Time)');
 //deletedata.get().then(function(querysnap){
 //    querysnap.forEach(function(doc) {
